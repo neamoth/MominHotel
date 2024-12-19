215e73b509ab77c163ea1f96b340e2177ad98d0e
Neamoth <Neamoth15-8202@diu.edu.bd>
2024-12-19 10:53:22 +0600

    updated menu display

--- js/home.js
+++ js/home.js
@@ -1,23 +1,66 @@
-const menu = {
-    Grill: 130,
-    Kabab: 180,
-    Nan: 30,
-    Singara: 5,
-    Chamucha: 5,
-    Beef_Biriyani: 180,
-    Chicken_Biriyani: 150,
-    Jhal_Fry: 120,
-    Chicken_Chap: 110
-}
+// Take the object as array
 
-const menuContainer = document.getElementById('menu');
-const foodName = document.createElement('span');
-foodName.className = 'foodName';
-foodName.textContent = Object.keys(menu.Beef_Biriyani);
+const menu = [
+    {
+        name: "Plain Rice & Chicken Jhal Fry",
+        price: 100
+    },
+    {
+        name: "Grill & 1 Butter Nan",
+        price: 150
+    },
+    {
+        name: "Chicken Chap & 1 Parata ",
+        price: 130
+    },
+    {
+        name: "Butter Nan",
+        price: 30
+    },
+    {
+        name: "Parata",
+        price: 20
+    },
+    {
+        name: "BBQ Kabab",
+        price: 180
+    },
+    {
+        name: "Singara Per Pieces",
+        price: 5
+    },
+    {
+        name: "Chamucha Per Piece",
+        price: 5
+    },
+    {
+        name: "Halim Standard",
+        price: 80
+    },
+    {
+        name: "Halim Premium",
+        price: 150
+    },
+]
 
-const foodPrice = document.createElement('span');
-foodPrice.className = 'foodPrice';
-foodPrice.textContent = menu.price;
+    const menuContainer = document.getElementById('menu');
 
-menuItem.appendChild(foodName);
-menuItem.appendChild(foodPrice);
+menu.forEach(item=>{
+
+    //Declearing a div for menu
+    const menuItem = document.createElement('div');
+    menuItem.className ='menuItem';
+
+    //Declearing a span for food name and price
+    const foodName = document.createElement('span');
+    foodName.className = 'foodName';
+    foodName.textContent = item.name;
+
+    const foodPrice = document.createElement('span');
+    foodPrice.className = 'foodPrice';
+    foodPrice.textContent = `${item.price}tk`;
+
+    menuItem.appendChild(foodName);
+    menuItem.appendChild(foodPrice);
+    menuContainer.appendChild(menuItem); 
+});
